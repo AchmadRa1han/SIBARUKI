68c9a3ffc11a5e213f5ab524f436359740a8019d
AchmadRa1han <noorlianalukman03@gmail.com>
2026-05-04 11:49:10 +0800

    chore: cleanup temporary scripts

--- check_counts.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$db = \Config\Database::connect();
-$res = $db->table('rtlh_rumah')
-          ->select('status_bantuan, COUNT(*) as total')
-          ->groupBy('status_bantuan')
-          ->get()->getResultArray();
-print_r($res);
